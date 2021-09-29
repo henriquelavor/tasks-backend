72a1e614bbc7ec4f08d0fe89a4f5e53669e113c5
henriquelavor <henriquelavor@gmail.com>
2021-09-29 16:46:35 -0400

    Testing.. true

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,7 +10,7 @@ public class DateUtilsTest {
 	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate date =  LocalDate.of(2022, 10, 01);
-		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
 	}
 	
 	@Test
